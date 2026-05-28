e00a946f1de8dc497d44c39d9662105b5e373980
pinoen <emiliopino.ar@gmail.com>
2026-05-27 22:02:20 -0300

    feat: implement ClientsService with CRUD operations and error handling, add validation to CreateClientDto

--- api/src/clients/clients.service.ts
+++ api/src/clients/clients.service.ts
@@ -1,26 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Client } from './entities/client.entity';
 
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientRepository: Repository<Client>,
+  ) { }
+  async create(createClientDto: CreateClientDto) {
+    const client = this.clientRepository.create(createClientDto)
+    await this.clientRepository.save(client)
+    return client;
   }
 
-  findAll() {
-    return `This action returns all clients`;
+  async findAll() {
+    return await this.clientRepository.find()
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} client`;
+  async findOne(id: number) {
+    const client = await this.clientRepository.findOne({ where: { id }, relations: ['projects'] })
+
+    if (!client) {
+      throw new NotFoundException(`Client with the id ${id} was not found.`)
+    }
+
+    return client;
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  async update(id: number, updateClientDto: UpdateClientDto) {
+    const client = await this.findOne(id)
+    const updatedClient = Object.assign(client, updateClientDto)
+
+    await this.clientRepository.save(updatedClient)
+    return updatedClient;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} client`;
+  async remove(id: number) {
+    const client = await this.findOne(id)
+    await this.clientRepository.remove(client)
+
+    return client;
   }
 }
--- api/src/clients/dto/create-client.dto.ts
+++ api/src/clients/dto/create-client.dto.ts
@@ -1,1 +1,6 @@
-export class CreateClientDto {}
+import { IsString } from "class-validator";
+
+export class CreateClientDto {
+  @IsString()
+  name!: string
+}
