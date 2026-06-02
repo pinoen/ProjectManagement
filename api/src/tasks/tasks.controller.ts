48d5996db631235adcfdb68f0a059ac05842401e
pinoen <emiliopino.ar@gmail.com>
2026-06-02 18:27:59 -0300

    feat: update task removal logic to set status to INACTIVE instead of deleting

--- api/src/tasks/tasks.controller.ts
+++ api/src/tasks/tasks.controller.ts
@@ -5,7 +5,7 @@ import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Controller('tasks')
 export class TasksController {
-  constructor(private readonly tasksService: TasksService) {}
+  constructor(private readonly tasksService: TasksService) { }
 
   @Post()
   create(@Body() createTaskDto: CreateTaskDto) {
--- api/src/tasks/tasks.service.ts
+++ api/src/tasks/tasks.service.ts
@@ -2,7 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Task } from './entities/task.entity';
+import { Task, TaskStatus } from './entities/task.entity';
 import { Repository } from 'typeorm';
 import { Project } from '../projects/entities/project.entity';
 
@@ -47,7 +47,9 @@ export class TasksService {
 
   async remove(id: number) {
     const task = await this.findOne(id)
-    await this.taskRepository.remove(task)
+
+    task.status = TaskStatus.INACTIVE
+    await this.taskRepository.save(task)
     return task;
   }
 }
